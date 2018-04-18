a5ad715ff4ed48fa689590abf97c5020d8289ff8
lee <605165376@qq.com>
2018-04-18 15:48:07 +0800

    删除

--- 手机.py
+++
@@ -1,9 +0,0 @@
-
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-
-mobile_emulation = {"deviceName":"Apple iPhone 6"}
-chrome_options = Options()
-chrome_options.add_experimental_option("mobileEmulation", mobile_emulation)
-driver = webdriver.Chrome(chrome_options=chrome_options)
-driver.get("http://www.baidu.com")
