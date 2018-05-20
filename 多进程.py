9a6759763516d187f2c2b0fcdf2912e21b98692a
LEE <605165376@qq.com>
2018-05-20 23:45:59 +0800

    多进程使用

---
+++ 多进程.py
@@ -0,0 +1,65 @@
+from multiprocessing import Process
+import os
+import time
+'''multiprocessing'''
+# def run_proc(name):
+#     time.sleep(3)
+#     print('run child process %s(%s)...'%(name,os.getpid()))
+#
+# if __name__=='__main__':
+#     print('parent process %s'%(os.getpid()))
+#     processes=list()
+#     for i in range(5):
+#         p=Process(target=run_proc,args=('test',))
+#         print('Process will start.')
+#         p.start()
+#         processes.append(p)
+#     for p in processmes:
+#         p.join()
+#     print('Process end.')
+'''Pool'''
+#import multiprocessing
+#import time
+#def func(msg,i):
+#    print('msg:',msg)
+#    time.sleep(i)
+#    print('end')
+#if __name__=='__main__':
+#    pool=multiprocessing.Pool(processes=4)
+#    for i in range(5):
+#        msg='hello %d'%(i)
+#        pool.apply(func,(msg,i,)) #apply阻塞 appy_async不阻塞
+#    print('hhahahha~~~~')
+#    pool.close()
+#    pool.join() #在close后面
+
+'''Queue'''
+# from multiprocessing import Process,Queue
+# import random,time
+# def wt(q):
+#     u=['1','2','3']
+#     for i in u:
+#         q.put(i)
+#         time.sleep(random.random())
+#
+# def rt(q):
+#     while(True):
+#         print(q.get())
+#
+# if __name__== '__main__':
+#     q=Queue()
+#     pw=Process(target=wt,args=(q,))
+#     pr = Process(target=rt, args=(q,))
+#     pw.start()
+#     pr.start()
+#     pw.join()
+#     pr.terminate()
+
+
+
+
+
+
+
+
+
